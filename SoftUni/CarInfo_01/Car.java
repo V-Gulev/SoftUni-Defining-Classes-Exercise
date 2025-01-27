153b30cf36d478dff939c1b84752a478c1e5bdfc
V-Gulev <velizargulev@abv.bg>
2025-01-27 20:03:29 +0200

    CarInfo_01

--- SoftUni/CarInfo_01/Car.java
+++ SoftUni/CarInfo_01/Car.java
@@ -1,5 +1,18 @@
 package SoftUni.CarInfo_01;
 
 public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP.", brand, model, horsePower);
+    }
 
 }
--- SoftUni/CarInfo_01/Main.java
+++ SoftUni/CarInfo_01/Main.java
@@ -1,4 +1,26 @@
 package SoftUni.CarInfo_01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        List<Car> cars = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = sc.nextLine().split("\\s+");
+            String brand = input[0];
+            String model = input[1];
+            int hp = Integer.parseInt(input[2]);
+
+            cars.add(new Car(brand, model, hp));
+
+        }
+
+        cars.forEach(car -> System.out.println(car.carInfo()));
+
+    }
 }
